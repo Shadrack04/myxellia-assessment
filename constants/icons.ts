75edbaa1028326ae2e366a4c2f37df146bcea2a6
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-08-16 13:11:13 +0100

    install dependencies

---
+++ constants/icons.ts
@@ -0,0 +1,13 @@
+import bellIcon from "@/public/assets/svg/bell.svg";
+import budgetIcon from "@/public/assets/svg/budget.svg";
+import calenderIcon from "@/public/assets/svg/calender.svg";
+import messageIcon from "@/public/assets/svg/message.svg";
+import myxelliaLogo from "@/public/assets/svg/myxellia-logo.svg";
+
+export const icons = {
+  bellIcon,
+  budgetIcon,
+  calenderIcon,
+  messageIcon,
+  myxelliaLogo,
+};
