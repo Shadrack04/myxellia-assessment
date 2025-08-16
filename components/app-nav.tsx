2714cde0fe4768fca63901286b59f89de197e0fb
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-08-16 13:45:19 +0100

    create header

---
+++ components/app-nav.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Logo from "./logo";
+import Image from "next/image";
+import { icons } from "@/constants/icons";
+import Profile from "./profile";
+
+export default function AppNav() {
+  return (
+    <header className=" w-full h-34">
+      <div className="costume-padding bg-foreground w-full h-1/2 flex items-center justify-between">
+        <Logo />
+        <div className=" flex items-center justify-center gap-6">
+          <div className=" flex items-center justify-center gap-6">
+            <Image src={icons.bellIcon} alt="Notification icon" />
+            <Image src={icons.budgetIcon} alt="Notification icon" />
+            <Image src={icons.calenderIcon} alt="Notification icon" />
+            <Image src={icons.messageIcon} alt="Notification icon" />
+          </div>
+          <Profile />
+        </div>
+      </div>
+      <div className="costume-padding border-b-2 w-full h-1/2"></div>
+    </header>
+  );
+}
---
+++ components/profile.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div className=" size-10 flex items-center justify-center rounded-full bg-background text-xl">
+      D
+    </div>
+  );
+}
