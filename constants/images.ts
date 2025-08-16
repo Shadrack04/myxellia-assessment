5e38c4c3f2541d4d4ecadea827d57b089fce7588
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-08-17 00:43:26 +0100

    render the three images down

---
+++ constants/images.ts
@@ -0,0 +1,9 @@
+import image1 from "@/public/assets/images/image1.webp";
+import image2 from "@/public/assets/images/image2.webp";
+import image3 from "@/public/assets/images/image3.webp";
+
+export const images = {
+  image1,
+  image2,
+  image3,
+};
